0b7898176d14bd5bb226961d5deab2956edc3de8
Kunmun <kunmun@aospa.co>
2025-02-07 09:30:31 +0530

    dotfiles: Update to 2.9.8 RC2
    Change-Id: Idda221eb220a123b1eb02a29702ab6586b239329

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#   ____                                          _      
-#  / ___| __ _ _ __ ___   ___ _ __ ___   ___   __| | ___ 
+#   ____                                          _
+#  / ___| __ _ _ __ ___   ___ _ __ ___   ___   __| | ___
 # | |  _ / _` | '_ ` _ \ / _ \ '_ ` _ \ / _ \ / _` |/ _ \
 # | |_| | (_| | | | | | |  __/ | | | | | (_) | (_| |  __/
 #  \____|\__,_|_| |_| |_|\___|_| |_| |_|\___/ \__,_|\___|
 #
 #
 
-if [ -f ~/.cache/gamemode ] ;then
+if [ -f $HOME/.config/ml4w/settings/gamemode-enabled ]; then
     hyprctl reload
-    rm ~/.cache/gamemode
+    rm $HOME/.config/ml4w/settings/gamemode-enabled
     notify-send "Gamemode deactivated" "Animations and blur enabled"
 else
     hyprctl --batch "\
@@ -20,6 +20,6 @@ else
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-	touch ~/.cache/gamemode
+    touch $HOME/.config/ml4w/settings/gamemode-enabled
     notify-send "Gamemode activated" "Animations and blur disabled"
 fi
