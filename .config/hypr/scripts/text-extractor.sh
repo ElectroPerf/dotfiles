97bd203ade2d2e02d8d1129f602a1a34caa109bf
Kunmun <kunmun@aospa.co>
2026-02-26 20:59:08 +0530

    dotfiles: update to 2.11.0
    Change-Id: Idd06c0a9302fe8c2c25aeffc674c918ea120057a

--- .config/hypr/scripts/text-extractor.sh
+++ .config/hypr/scripts/text-extractor.sh
@@ -12,6 +12,7 @@ safe_kill() { [[ -n "${1:-}" ]] && kill "$1" 2>/dev/null || true; }
 cleanup() { safe_kill "$PICKER_PID"; }
 trap cleanup EXIT INT TERM
 
+# Check dependencies
 check_deps() {
   local missing_dependencies=()
   for dep in "${DEPS[@]}"; do command -v "$dep" >/dev/null 2>&1 || missing_dependencies+=("$dep"); done
@@ -20,7 +21,43 @@ check_deps() {
   fi
 }
 
+# Check if command exists
+_checkCommandExists() {
+    cmd="$1"
+    if ! command -v "$cmd" >/dev/null; then
+        echo 1
+        return
+    fi
+    echo 0
+    return
+}
+
 check_deps
+
+# Arch
+if [[ $(_checkCommandExists "pacman") == 0 ]]; then
+    OCR_LANGUAGE_LIST="$(pacman -Qq | grep -iE "tesseract-(ocr|data|langpack)*-" | awk -F '-' '{print $NF}')"
+# Fedora
+elif [[ $(_checkCommandExists "dnf") == 0 ]]; then
+    OCR_LANGUAGE_LIST="$(dnf list --installed | grep -iE "tesseract-(ocr|data|langpack)*-" | awk -F '-' '{print $NF}')"
+# Opensuse
+else
+    OCR_LANGUAGE_LIST="$(zypper se -i | grep -iE "tesseract-(ocr|data|langpack)*-" | awk -F '-' '{print $NF}')"
+fi
+
+argc() { echo $#; }
+rofi_cmd() {
+    rofi -dmenu -replace -config ~/.config/rofi/config-ocr-lang.rasi -i -no-show-icons -l 3 -width 30 -p "Select the OCR language"
+}
+
+if [ "$(argc $OCR_LANGUAGE_LIST)" -gt 1 ]; then
+    OCR_LANGUAGE=$(echo -e "$OCR_LANGUAGE_LIST" | rofi_cmd)
+    sleep 0.5 || true
+fi
+
+if [ -z "$OCR_LANGUAGE" ]; then
+    OCR_LANGUAGE="eng"
+fi
 
 hyprpicker -r -z &
 PICKER_PID=$!
@@ -32,6 +69,6 @@ cleanup
 
 grim -g "$REGION" - \
   | magick - -colorspace Gray -normalize -contrast-stretch 2% -sharpen 0x1.0 -resize 200% png:- \
-  | tesseract - stdout -l eng --psm 6 \
+  | tesseract - stdout -l $OCR_LANGUAGE --psm 6 \
   | wl-copy \
   || die "Failed to capture or process text"
